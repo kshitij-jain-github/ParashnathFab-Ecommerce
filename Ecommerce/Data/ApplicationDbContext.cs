53a3b29087eb77f8846341b63558656ee309bd55
Kshitij Jain <jainkshitij1530@gmail.com>
2024-05-11 11:24:03 +0530

    ConnectionString & DBCONTEXT

---
+++ Ecommerce/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}

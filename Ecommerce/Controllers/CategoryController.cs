982845af927fd648cddd24d295921ae096d226d8
Kshitij Jain <jainkshitij1530@gmail.com>
2024-05-11 11:57:08 +0530

    createCategory

--- Ecommerce/Controllers/CategoryController.cs
+++ Ecommerce/Controllers/CategoryController.cs
@@ -16,5 +16,17 @@ namespace Ecommerce.Controllers
             List<Category> objCategoryList =_db.Categories.ToList();
             return View(objCategoryList);
         }
-    }
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+        [HttpPost]
+        public IActionResult Create(Category obj)
+		{
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+			return RedirectToAction("index","category");
+		}
+	}
 }

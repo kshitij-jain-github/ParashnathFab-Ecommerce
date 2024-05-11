c5033c3e5f7c6ee21010a3541d1156351bbdda85
Kshitij Jain <jainkshitij1530@gmail.com>
2024-05-11 11:43:00 +0530

    displayCategory

--- Ecommerce/Controllers/CategoryController.cs
+++ Ecommerce/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ecommerce.Data;
+using Ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Ecommerce.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList =_db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }

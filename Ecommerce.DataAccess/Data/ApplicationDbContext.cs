fdb4291074ec5124f323b0d39c72fbff3e094aaf
Kshitij Jain <jainkshitij1530@gmail.com>
2024-05-12 09:56:37 +0530

    Added Product classes

--- Ecommerce.DataAccess/Data/ApplicationDbContext.cs
+++ Ecommerce.DataAccess/Data/ApplicationDbContext.cs
@@ -10,5 +10,7 @@ namespace Ecommerce.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
     }
 }
---
+++ Ecommerce.Model/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace Ecommerce.Model
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        [Required]
+        public string Product_Name { get; set; }
+        [Required]
+        public string Fabric { get; set; }
+        [Required]
+        public string Size { get; set; }
+        [Required]
+        public string Brand { get; set; }
+        public string Description { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        [Display(Name = "List Price")]
+        public double ListPrice { get; set; }
+        [Required]
+        [Range(1, 10000)]
+        [Display(Name = "Price for 1-50")]
+        public double Price { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        [Display(Name = "Price for 51-100")]
+        public double Price50 { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 100+")]
+        [Range(1, 10000)]
+        public double Price100 { get; set; }
+        
+    }
+}

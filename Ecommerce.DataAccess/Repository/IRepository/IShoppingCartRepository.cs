446b1cf68974f1bb4ee5f314f2b61f7a060f1892
Kshitij Jain <jainkshitij1530@gmail.com>
2024-06-11 08:09:11 +0530

    shoppingCartRepositoryPattern

--- Ecommerce.DataAccess/Repository/IRepository/IShoppingCartRepository.cs
+++ Ecommerce.DataAccess/Repository/IRepository/IShoppingCartRepository.cs
@@ -7,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace Ecommerce.DataAccess.Repository.IRepository
 {
-    public interface IApplicationUserRepository:IRepository<ApplicationUser>
+    public interface IShoppingCartRepository:IRepository<ShoppingCart>
     {
+        void Update(ShoppingCart obj);
+  
     }
 }
---
+++ Ecommerce.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,31 @@
+﻿using Ecommerce.Data;
+using Ecommerce.DataAccess.Repository.IRepository;
+using Ecommerce.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(ShoppingCart obj)
+        {
+            _db.ShoppingCarts.Update(obj);
+        }
+    }
+}

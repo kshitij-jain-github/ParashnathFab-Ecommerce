5f3b2c9c78983b30bd495d0b5c1e25e4c070c3a8
Kshitij Jain <jainkshitij1530@gmail.com>
2024-06-13 08:28:58 +0530

    shopping cat vm

---
+++ Ecommerce/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,59 @@
+﻿using Ecommerce.DataAccess.Repository.IRepository;
+using Ecommerce.Model;
+using Ecommerce.Model.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Ecommerce.Areas.Customer.Controllers
+{
+	[Area("Customer")]
+	[Authorize]
+	public class CartController : Controller
+    {
+
+		private readonly IUnitOfWork _unitOfWork;
+ 		public ShoppingCartVM ShoppingCartVM { get; set; }
+		public CartController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+ 		}
+		public IActionResult Index()
+		{
+			var claimsIdentity = (ClaimsIdentity)User.Identity;
+			var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+			ShoppingCartVM = new ShoppingCartVM()
+			{
+				ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value,
+				includeProperties: "Product"),
+ 			};
+			foreach(var cart in ShoppingCartVM.ShoppingCartList)
+			{
+				double price = GetPriceBasedOnQuality(cart);
+				ShoppingCartVM.OrderTotal= price*cart.Count;
+			}
+			return View(ShoppingCartVM);
+        }
+
+		private double GetPriceBasedOnQuality(ShoppingCart shoppingCart)
+
+		{
+			if(shoppingCart.Count<=50)
+			{
+				return shoppingCart.Product.Price;
+			}
+			else
+			{
+				if(shoppingCart.Count<=100)
+				{
+					return shoppingCart.Product.Price50;
+				}
+				else
+				{
+					return shoppingCart.Product.Price100;
+				}
+			}
+		}
+    }
+}

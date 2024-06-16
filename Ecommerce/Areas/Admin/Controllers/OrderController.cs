39ec3ea2bde0ba61aba64da946a3993c69e3a83b
Kshitij Jain <jainkshitij1530@gmail.com>
2024-06-16 18:02:53 +0530

    order-detals-view/functionality

---
+++ Ecommerce/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,56 @@
+﻿using Ecommerce.DataAccess.Repository.IRepository;
+using Ecommerce.Model;
+using Ecommerce.Model.ViewModel;
+using Ecommerce.Utility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(int orderId)
+        {
+            OrderVM orderVM= new OrderVM()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == orderId, includeProperties: "ApplicationUser"),
+                OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderId == orderId, includeProperties: "Product"),
+            };
+            return View(orderVM);
+
+         }
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll(string status) { 
+        IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties:"ApplicationUser").ToList();
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+            return Json(new { data = objOrderHeaders });
+                }
+        #endregion
+    }
+}
